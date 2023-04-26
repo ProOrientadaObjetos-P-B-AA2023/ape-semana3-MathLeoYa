1eb4e20343872f9028f5967e2564ecba52ff65fa
MathLeoYa <mateoleonardo1616@gmail.com>
2023-04-26 17:22:05 -0500

    semana3

--- problema1/problema1/src/problema1/Problema1.java
+++ problema1/problema1/src/problema1/Problema1.java
@@ -1,4 +1,3 @@
-
 package problema1;
 class Terreno{
     private double costo_terreno;
@@ -7,7 +6,6 @@ class Terreno{
     private double area;
     private double valorterreno;
     public Terreno(){}
-
     public Terreno(double ancho, double largo, double valorterreno) {
         this.ancho = ancho;
         this.largo = largo;
@@ -17,15 +15,12 @@ class Terreno{
     public void setAncho(double ancho) {
         this.ancho = ancho;
     }
-
     public void setLargo(double largo) {
         this.largo = largo;
     }
-
     public void setValorterreno(double valorterreno) {
         this.valorterreno = valorterreno;
     }
-    
     //calcular area del terreno
     public void calcularAra(){
         this.area=this.ancho*this.largo;
@@ -34,28 +29,22 @@ class Terreno{
     public void calcularCosto(){
         this.costo_terreno=this.area*this.valorterreno;
     }
-
     //GET
     public double getCosto_terreno() {
         return this.costo_terreno;
     }
-
     public double getAncho() {
         return this.ancho;
     }
-
     public double getLargo() {
         return this.largo;
     }
-
     public double getArea() {
         return this.area;
     }
-
     public double getValorterreno() {
         return this.valorterreno;
     }
-    
     public String toString(){
         String msj=String.format("DATOS DEL TERRENO:"
                 + "\nAncho del terreno: %.2f " 
@@ -66,18 +55,12 @@ class Terreno{
                 this.getAncho(),this.getLargo(),this.getValorterreno(),this.getArea(),this.getCosto_terreno());
         return msj;
     }
-    
-    
 }
 public class Problema1 {
-
     public static void main(String[] args) {
         Terreno terreno1=new Terreno(10,2,2);
         terreno1.calcularAra();
         terreno1.calcularCosto();
-        System.out.println(terreno1);
-        
-        
+        System.out.println(terreno1); 
     }
-    
 }
